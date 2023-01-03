e7751b387277d7be33430a9d83acaf6ce3ac6c23
Albina <albisha900325@gmail.com>
2023-01-03 23:54:22 +0500

    Задача 2/2

--- Seminar001_zadacha2/Program.cs
+++ Seminar001_zadacha2/Program.cs
@@ -1,5 +1,13 @@
 ﻿Console.WriteLine("введите числа: ");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b) Console.WriteLine("a = max, b = min");
-else Console.WriteLine("b = max, a = min");
+int max = a;
+int min = b;
+if (a > b) max = a;
+if (a > b) min = b;
+if (a < b) max = b;
+if (a < b) min = a;
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min= ");
+Console.WriteLine(min);
