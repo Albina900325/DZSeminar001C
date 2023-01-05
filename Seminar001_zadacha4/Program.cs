58cf318ca4d2a518a1cd890e698b0c420714c074
Albina <albisha900325@gmail.com>
2023-01-05 23:53:27 +0500

    Задача 4

--- Seminar001_zadacha4/Program.cs
+++ Seminar001_zadacha4/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите числа: ");
+int a = Convert.ToInt32(Console.ReadLine ());
+int b = Convert.ToInt32(Console.ReadLine ());
+int c = Convert.ToInt32(Console.ReadLine ());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max= ");
+Console.WriteLine(max);
